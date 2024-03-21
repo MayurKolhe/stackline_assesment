c83a1728a4bf3929fc902b7956e7de042cbbc760
Mayur Kolhe <110931840+MayurKolhe@users.noreply.github.com>
2024-03-21 21:55:25 +0000

    fixed tailwind css issue commiting changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/stackline_logo.svg";
 const NavBar = () => {
   return (
     <div className="bg-blue-900 h-16 flex items-center">
-      <img src={Logo} alt="Stackline Logo" className="h-6 ml-4" />
+      <img src={Logo} alt="Stackline Logo" className="w-18 h-18"/>
     </div>
   );
 }
